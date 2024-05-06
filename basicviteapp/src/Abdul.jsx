74a7d17249b786c0235e731bbe1fbc8a71635dd2
abdul894 <a.rehmanshabbir1@gmail.com>
2024-05-06 11:14:52 +0500

    learned how to initite a react app using CRA and vite.

---
+++ basicviteapp/src/Abdul.jsx
@@ -0,0 +1,7 @@
+function Abdul() {
+    return(
+        <h1>Abdul's vite react app</h1>
+    )
+}
+
+export default Abdul;
---
+++ basicviteapp/src/App.jsx
@@ -0,0 +1,13 @@
+import Abdul from "./Abdul";
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite react app</h1>
+      <Abdul />
+    </>
+  )
+}
+
+export default App
