96553e53836eb7ccdc4848730a3b9068c9436048
abdul894 <a.rehmanshabbir1@gmail.com>
2024-05-17 15:22:12 +0500

    adding props to make dumb Components

--- tailwindwithReact/src/App.jsx
+++ tailwindwithReact/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Card from './Components/Card'
 import './App.css'
 
 function App() {
@@ -8,7 +7,10 @@ function App() {
 
   return (
     <>
-      
+      <h1 className="text-3xl bg-green-500 p-3 rounded-md">
+        Vite with tailwind</h1>
+        <Card username="Hina" img="https://images.pexels.com/photos/22644030/pexels-photo-22644030/free-photo-of-a-woman-sitting-on-a-chair-and-smiling.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" post="Staff engineer" city="Islamad"/>
+        <Card username="Hina" img="https://images.pexels.com/photos/22644030/pexels-photo-22644030/free-photo-of-a-woman-sitting-on-a-chair-and-smiling.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" post="Staff engineer" city="Islamad"/>
     </>
   )
 }
