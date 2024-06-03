03279b33167495e59d5f3ecc5a7da1ec4d9cbea6
abdul894 <a.rehmanshabbir1@gmail.com>
2024-06-03 12:39:15 +0500

    currency converter

--- currencyconverter/src/App.jsx
+++ currencyconverter/src/App.jsx
@@ -43,6 +43,26 @@ function App() {
                 selectdCurrency={from}
               />
             </div>
+            <div className='relative w-full h-0.5'>
+              <button
+                className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
+                onClick={swap}
+              >Swap</button>
+            </div>
+            <div className='w-full mb-1'>
+              <InputBox
+              label={to}
+              currencyOptions={options}
+              amount={convertedAmount}
+              onCurrencyChange={(currency) => setTo(currency)}
+              seletedCurrency={to}
+              amountDisabled
+              />
+            </div>
+            <button
+            type='submit'
+            className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg'
+            >Convert {from.toUpperCase()} to {to.toUpperCase()}</button>
           </form>
 
         </div>
