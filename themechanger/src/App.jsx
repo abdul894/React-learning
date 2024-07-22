d284f59381c4f4f070e5f6f34e0d4159e20d4dc5
abdul894 <a.rehmanshabbir1@gmail.com>
2024-07-21 22:33:39 -0700

    .

--- themechanger/src/App.jsx
+++ themechanger/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 import ThemeBtn from './components/ThemeBtn'
+import { ThemeProvider } from './Context/Theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState('light')
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('dark', 'light')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    <>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
       <div className='flex flex-wrap min-h-screen items-center'>
         <div className='w-full'>
           <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
@@ -19,9 +33,8 @@ function App() {
             <Card />
           </div>
         </div>
-
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
