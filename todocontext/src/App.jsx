62d58d8dfc35b65912c333e5cdc1fae1c96782a2
abdul894 <a.rehmanshabbir1@gmail.com>
2024-07-31 11:31:58 +0500

    .

--- todocontext/src/App.jsx
+++ todocontext/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './Contexts'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <TodoProvider>
       <div className='bg-blue-500'>Hello world</div>
-    </>
+    </TodoProvider>
   )
 }
 
