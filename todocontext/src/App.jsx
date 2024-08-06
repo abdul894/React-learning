e187bfee878eb39e032e9874ae53f2ab4b078494
abdul894 <a.rehmanshabbir1@gmail.com>
2024-08-06 11:42:13 +0500

    .

--- todocontext/src/App.jsx
+++ todocontext/src/App.jsx
@@ -11,7 +11,10 @@ function App() {
     setTodos((prev) => [ {id: Date.now(), ...todo}, ...prev])
   }
 
-  
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id 
+      === todo.id ? todo : prevTodo)))
+  }
 
   return (
     <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
