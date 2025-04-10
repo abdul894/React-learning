bcbdb676f75383fbd0b651d208723cd3502a398a
abdul894 <a.rehmanshabbir1@gmail.com>
2025-04-10 10:28:56 +0500

    simple cards

--- pdas/src/App.jsx
+++ pdas/src/App.jsx
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./assets/alexa.png";
 import CortanaImage from "./assets/cortana.png";
@@ -6,11 +7,43 @@ import SiriImage from "./assets/siri.png";
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistant</div>
+            <section className='hero is-info'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistant</p>
+                </div>
+            </section>
 
-            <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImage} />
-            <ProfileCard title='Cortana' handle='@cortana32' image={CortanaImage} />
-            <ProfileCard title='Siri' handle='@siri01' image={SiriImage} />
+            <div className='container'>
+                <div className='section'>
+                    <div className='columns'>
+                        <div className='column is-3'>
+                            <ProfileCard 
+                                title='Alexa' 
+                                handle='@alexa99' 
+                                image={AlexaImage}
+                                description="Alexa was created by Amazon to help you buy things." 
+                            />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard 
+                                title='Cortana' 
+                                handle='@cortana32' 
+                                image={CortanaImage} 
+                                description="Cortana was made by Microsoft."
+                            />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard 
+                                title='Siri' 
+                                handle='@siri01' 
+                                image={SiriImage} 
+                                description="Siri was made by Apple"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
         </div>
     );
 }
