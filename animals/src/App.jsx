84776f373f68620513af4192b0a946f956b9bc1d
abdul894 <a.rehmanshabbir1@gmail.com>
2025-04-22 09:04:41 +0500

    adding svgs

--- animals/src/App.jsx
+++ animals/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
 
 function getRandomAnimal() {
     const animals = ['cat', 'horse', 'gator', 'bord', 'cow', 'dog'];
@@ -10,10 +11,14 @@ function App() {
 
     const [animals, setAnimals] = useState([]);
 
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />;
+    });
+
     return (
         <div>
             <button onClick={() => setAnimals([...animals, getRandomAnimal()])}>Add Animal</button>
-            <div> {animals} </div>
+            <div> {renderedAnimals} </div>
         </div>
     );
 }
